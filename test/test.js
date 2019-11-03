62d187fe23498760380784a189ed4d83eeebd786
tones31 <tones31@users.noreply.github.com>
2019-11-02 22:17:48 -0400

    test folders
    - organized test folders

--- test/test.js
+++ test/test.js
@@ -2,13 +2,46 @@ const Fs = require('fs');
 const Assert = require('assert');
 const Builder = require('./../index');
 
-const jsfile1 = "./test/in1.js";
-const jsfile2 = "./test/in2.js";
-const jsfileout = "./test/out.min.js";
+const jsfile1 = "./test/js/in1.js";
+const jsfile2 = "./test/js/in2.js";
+const jsfileout = "./test/js/out.min.js";
+const jsoutput = "const a=1,b=2;";
+const jsconfig = {
+    name: 'JS Test',
+    version: '1.0.0',
+    input: [jsfile1, jsfile2],
+    output: jsfileout,
+    logger: false
+};
 
-const cssfile1 = "./test/in1.css";
-const cssfile2 = "./test/in2.css";
-const cssfileout = "./test/out.min.css";
+const cssfile1 = "./test/css/in1.css";
+const cssfile2 = "./test/css/in2.css";
+const cssfileout = "./test/css/out.min.css";
+const cssoutput = "html{color:red}body{color:#000}";
+const cssconfig = {
+    name: 'CSS Test',
+    version: '1.0.0',
+    input: [cssfile1, cssfile2],
+    output: cssfileout,
+    logger: false
+};
+
+const htmlfilebase = "./test/html/base.html"
+const htmlfile1 = "./test/html/in1.html";
+const htmlfile2 = "./test/html/in2.html";
+const htmlfileout = "./test/html/out.html"
+const htmloutput = '<!doctype html><html lang="en"><div id="inject_1"></div><div id="inject_2"></div></html>';
+const htmlconfig = {
+    name: 'HTML Test',
+    version: '1.0.0',
+    base: htmlfilebase,
+    input: [
+        {filepath: htmlfile1, tag: 'inject1'},
+        {filepath: htmlfile2, tag: 'inject2'}
+    ],
+    output: htmlfileout,
+    logger: false
+};
 
 function deleteFiles(){
     try {
@@ -16,6 +49,8 @@ function deleteFiles(){
         Fs.unlinkSync(jsfileout);
         Fs.accessSync(cssfileout);
         Fs.unlinkSync(cssfileout);
+        Fs.accessSync(htmlout);
+        Fs.unlinkSync(htmlout);
     }
     catch(e){
         
@@ -25,53 +60,36 @@ function deleteFiles(){
 deleteFiles();
 
 it('minifies JS files', () => {
-    let builder = new Builder.JsBuilder({
-        name: 'JS Test',
-        version: '1.0.0',
-        input: [jsfile1, jsfile2],
-        output: jsfileout,
-        logger: false
-    });
+    let builder = new Builder.JsBuilder(jsconfig);
     builder.run();
     let result = Fs.readFileSync(jsfileout).toString();
-    Assert.strictEqual(result, "const a=1,b=2;");
+    Assert.strictEqual(result, jsoutput);
 });
 
 it('minifies CSS files', () => {
-    let builder = new Builder.CssBuilder({
-        name: 'CSS Test',
-        version: '1.0.0',
-        input: [cssfile1, cssfile2],
-        output: cssfileout,
-        logger: false
-    });
+    let builder = new Builder.CssBuilder(cssconfig);
     builder.run();
     let result = Fs.readFileSync(cssfileout).toString();
-    Assert.strictEqual(result, "html{color:red}body{color:#000}");
+    Assert.strictEqual(result, cssoutput);
 });
 
-it('minifies JS and CSS files from a config', () => { 
+it('injects HTML files', async () => {
+    let builder = new Builder.HtmlBuilder(htmlconfig);
+    await builder.run();
+    let result = Fs.readFileSync(htmlfileout).toString();
+    Assert.strictEqual(result, htmloutput);
+});
+
+it('minifies JS and CSS and injects HTML from a config', async () => { 
     deleteFiles();
-    let jsconfig = {
-        name: 'JS Test',
-        version: '1.0.0',
-        input: [jsfile1, jsfile2],
-        output: jsfileout,
-        logger: false
-    };
-    let cssconfig = {
-        name: 'CSS Test',
-        version: '1.0.0',
-        input: [cssfile1, cssfile2],
-        output: cssfileout,
-        logger: false
-    };
-    let configs = [jsconfig, cssconfig];
+    let configs = [jsconfig, cssconfig, htmlconfig];
     let build = new Builder.Build(configs, false);
-    build.run();
+    await build.run();
 
     let jsresult = Fs.readFileSync(jsfileout).toString();
-    Assert.strictEqual(jsresult, "const a=1,b=2;");
+    Assert.strictEqual(jsresult, jsoutput);
     let cssresult = Fs.readFileSync(cssfileout).toString();
-    Assert.strictEqual(cssresult, "html{color:red}body{color:#000}");
+    Assert.strictEqual(cssresult, cssoutput);
+    let result = Fs.readFileSync(htmlfileout).toString();
+    Assert.strictEqual(result, htmloutput);
 });
