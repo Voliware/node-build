91a36922e0f2d4d50be57f6aebf22d01e29035de
tones31 <anthagostino@gmail.com>
2020-02-23 13:23:27 -0500

    1.3.1
    - added support for HtmlBuilder
    - rewrote FileBuilder build
    - all minifiers now use node-minify-transform

--- test/test.js
+++ test/test.js
@@ -3,63 +3,6 @@ const Path = require('path');
 const Assert = require('assert');
 const Builder = require('./../index');
 
-const jsfile1 = "./test/js/in1.js";
-const jsfile2 = "./test/js/in2.js";
-const jsfileout = "./test/js/out.min.js";
-const jsoutput = "async function bar(a){await foo(a)}class Foo{bar(){let a={a:1,b:{}};return a.b.a=1,a}}";
-const jsconfig = {
-    name: 'JS Test',
-    version: '1.0.0',
-    input: [jsfile1, jsfile2],
-    output: jsfileout,
-    minify: true,
-    gzip: true,
-    logger: false
-};
-
-const cssfile1 = "./test/css/in1.css";
-const cssfile2 = "./test/css/in2.css";
-const cssfileout = "./test/css/out.min.css";
-const cssoutput = "html{color:red}body{color:#000}";
-const cssconfig = {
-    name: 'CSS Test',
-    version: '1.0.0',
-    input: [cssfile1, cssfile2],
-    output: cssfileout,
-    minify: true,
-    gzip: true,
-    logger: false
-};
-
-const htmlstart = Path.join(__dirname, "/html/start.html");
-const htmlcontent = Path.join(__dirname, "/html/content.html");
-const htmlend = Path.join(__dirname, "/html/end.html");
-const htmlfileout = Path.join(__dirname, "/html/out.html");
-const htmlfileappend = Path.join(__dirname, "/html/append.html");
-const htmlfileprepend = Path.join(__dirname, "/html/prepend.html");
-const htmloutput = `<!doctype html><html lang="en"><body><!-- templates --><div id="templates"></div></body><script src="app.min.js"></script><!-- scripts --><footer></footer>`;
-const htmlconfig = {
-    name: 'HTML Test',
-    version: '1.0.0',
-    input: [htmlstart, htmlcontent, htmlend],
-    output: htmlfileout,
-    minify: true,
-    gzip: true,
-    modifiers: {
-        // replace matches with contents
-        replace: [{match: 'div', contents: 'span'}],
-        // append contents after match
-        append: [{match: '<!-- templates -->', contents: htmlfileappend}],
-        // erase all matches (spaces, new lines)
-        erase: [
-            {match: '<head></head>'}
-        ],
-        // prepend contents before match
-        prepend: [{match: '<!-- scripts -->', contents: htmlfileprepend}],
-    },
-    logger: false
-};
-
 /**
  * Delete all test files
  */
@@ -79,6 +22,20 @@ function deleteFiles(){
 
 deleteFiles();
 
+const jsfile1 = "./test/js/in1.js";
+const jsfile2 = "./test/js/in2.js";
+const jsfileout = "./test/js/out.min.js";
+const jsoutput = "async function bar(a){await foo(a)}class Foo{bar(){let a={a:1,b:{}};return a.b.a=1,a}}";
+const jsconfig = {
+    name: 'JS Test',
+    version: '1.0.0',
+    logger: false,
+    input: [jsfile1, jsfile2],
+    output: jsfileout,
+    minify: true,
+    gzip: true
+};
+
 it('minifies JS files', async () => {
     let builder = new Builder.JsBuilder(jsconfig);
     await builder.run();
@@ -86,12 +43,53 @@ it('minifies JS files', async () => {
     Assert.strictEqual(result, jsoutput);
 });
 
-// it('minifies CSS files', () => {
-//     let builder = new Builder.CssBuilder(cssconfig);
-//     builder.run();
-//     let result = Fs.readFileSync(cssfileout).toString();
-//     Assert.strictEqual(result, cssoutput);
-// });
+const cssfile1 = "./test/css/in1.css";
+const cssfile2 = "./test/css/in2.css";
+const cssfileout = "./test/css/out.min.css";
+const cssoutput = "html{color:red}body{color:#000}";
+const cssconfig = {
+    name: 'CSS Test',
+    version: '1.0.0',
+    logger: false,
+    input: [cssfile1, cssfile2],
+    output: cssfileout,
+    minify: true,
+    gzip: true
+};
+
+it('minifies CSS files', async () => {
+    let builder = new Builder.CssBuilder(cssconfig);
+    await builder.run();
+    let result = Fs.readFileSync(cssfileout).toString();
+    Assert.strictEqual(result, cssoutput);
+});
+
+const htmlstart = Path.join(__dirname, "/html/start.html");
+const htmlcontent = Path.join(__dirname, "/html/content.html");
+const htmlend = Path.join(__dirname, "/html/end.html");
+const htmlfileout = Path.join(__dirname, "/html/out.html");
+const htmlfileappend = Path.join(__dirname, "/html/append.html");
+const htmlfileprepend = Path.join(__dirname, "/html/prepend.html");
+const htmloutput = `<!doctype html><html lang=en><div id=templates></div><footer></footer><script src=app.min.js></script>`;
+const htmlconfig = {
+    name: 'HTML Test',
+    version: '1.0.0',
+    logger: false,
+    input: [htmlstart, htmlcontent, htmlend],
+    output: htmlfileout,
+    minify: true,
+    gzip: true,
+    modifiers: {
+        // replace matches with contents
+        replace: [{match: 'div', contents: {string: 'span'}}],
+        // append contents after match
+        append: [{match: '<!-- templates -->', contents: {file: htmlfileappend}}],
+        // erase all matches
+        erase: [{match: '<head></head>'}],
+        // prepend contents before match
+        prepend: [{match: '<!-- scripts -->', contents: {file: htmlfileprepend}}],
+    }
+};
 
 it('builds HTML files', async () => {
     let builder = new Builder.HtmlBuilder(htmlconfig);
@@ -100,16 +98,16 @@ it('builds HTML files', async () => {
     Assert.strictEqual(result, htmloutput);
 });
 
-// it('minifies JS and CSS and builds HTML from a config', async () => { 
-//     deleteFiles();
-//     let configs = [jsconfig, cssconfig, htmlconfig];
-//     let build = new Builder.Build(configs, false);
-//     await build.run();
+it('minifies JS and CSS and builds HTML from a config', async () => { 
+    deleteFiles();
+    let configs = [jsconfig, cssconfig, htmlconfig];
+    let build = new Builder.Build(configs, false);
+    await build.run();
 
-//     let jsresult = Fs.readFileSync(jsfileout).toString();
-//     Assert.strictEqual(jsresult, jsoutput);
-//     let cssresult = Fs.readFileSync(cssfileout).toString();
-//     Assert.strictEqual(cssresult, cssoutput);
-//     let result = Fs.readFileSync(htmlfileout).toString();
-//     Assert.strictEqual(result, htmloutput);
-// });
+    let jsresult = Fs.readFileSync(jsfileout).toString();
+    Assert.strictEqual(jsresult, jsoutput);
+    let cssresult = Fs.readFileSync(cssfileout).toString();
+    Assert.strictEqual(cssresult, cssoutput);
+    let result = Fs.readFileSync(htmlfileout).toString();
+    Assert.strictEqual(result, htmloutput);
+});
